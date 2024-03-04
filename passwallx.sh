80f6360c2af0e5638d3a4812ef8a44f400457589
Reza Bahari <49598914+rbsdotnet@users.noreply.github.com>
2024-03-04 14:42:11 +0330

    Update passwallx.sh

--- passwallx.sh
+++ passwallx.sh
@@ -15,7 +15,7 @@ clear
 
 uci set network.wan.peerdns="0"
 
-uci set network.wan.dns='8.8.8.8 1.1.1.1'
+uci set network.wan.dns='8.8.8.8 8.8.4.4'
 
 uci set system.@system[0].zonename='Asia/Tehran'
 uci set system.@system[0].timezone='<+0330>-3:30'
